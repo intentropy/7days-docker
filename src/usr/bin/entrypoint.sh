1468efde8225b3b249c582da13491c66ae1eff17
Shane Hutter <shane@hal9001.intentropycs.com>
2022-09-14 15:38:04 +1000

    Keep checking until the server log is ready to tail

--- src/usr/bin/entrypoint.sh
+++ src/usr/bin/entrypoint.sh
@@ -2,5 +2,7 @@
 cd '/root/Steam/steamapps/common/7 Days to Die Dedicated Server'
 config_7days
 ./startserver.sh -configfile=serverconfig.xml &
-sleep 10
-tail -f /7days/7DaysToDieServer_Data/*.txt
+while true; do
+    sleep 10
+    tail -f /7days/7DaysToDieServer_Data/*.txt
+done
